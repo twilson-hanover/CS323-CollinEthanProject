afe2a7865721186c59335f67a8a46695f90ebac1
Ethan Monnin <monnine20@hanover.edu>
2018-05-23 11:32:58 -0400

    Documentation added to QuestionLibrary

--- app/src/main/java/com/example/zx/collinethanproject/QuestionLibrary.java
+++ app/src/main/java/com/example/zx/collinethanproject/QuestionLibrary.java
@@ -2,6 +2,7 @@ package com.example.zx.collinethanproject;
 
 public class QuestionLibrary {
 
+    //String for holding questions to be asked
     private String questions[] = {
             "When was Hanover College founded?",
         "When was there a tornado that struck our campus",
@@ -9,6 +10,7 @@ public class QuestionLibrary {
         "What's the name of the Computer Science building?"
     };
 
+    //String for holding choices for answering questions
     private String choices[][] = {
             {"1823","1911","1827","2003"},
             {"12 A.D.","1974","2011","There was no tornado"},
@@ -16,34 +18,38 @@ public class QuestionLibrary {
             {"Ogle","Circle K at 3am","Phi Delta Theta","Lynn"}
     };
 
+    //String of correct answers
     private String correct[] = {"1827","1974","Parker","Lynn"
     };
 
-    public String getQuestion(int a){
-        String q = questions[a];
+    //Function for getting questions
+    public String getQuestion(int x){
+        String q = questions[x];
         return q;
     }
 
-    public String getChoice1(int a){
-        String ans = choices[a][0];
-        return ans;
+
+    //Functions for getting question choices
+    public String getChoice1(int x){
+        String ans1 = choices[x][0];
+        return ans1;
     }
-    public String getChoice2(int a){
-        String ans = choices[a][1];
-        return ans;
+    public String getChoice2(int x){
+        String ans2 = choices[x][1];
+        return ans2;
     }
-    public String getChoice3(int a){
-        String ans = choices[a][2];
-        return ans;
+    public String getChoice3(int x){
+        String ans3 = choices[x][2];
+        return ans3;
     }
-    public String getChoice4(int a){
-        String ans = choices[a][3];
-        return ans;
+    public String getChoice4(int x){
+        String ans4 = choices[x][3];
+        return ans4;
     }
 
-    public String answer(int a){
-        String answer = correct[a];
-        return answer;
+    public String answer(int x){
+        String answercorrect = correct[x];
+        return answercorrect;
     }
 
 
