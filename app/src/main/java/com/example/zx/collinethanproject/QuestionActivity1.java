fe23af82a199b486bf142e963effcfc49c87a3f1
Ethan Monnin <monnine20@hanover.edu>
2018-05-23 10:38:55 -0400

    XML layout for question 1

---
+++ app/src/main/java/com/example/zx/collinethanproject/QuestionActivity1.java
@@ -0,0 +1,14 @@
+package com.example.zx.collinethanproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class QuestionActivity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question1);
+    }
+
+}
