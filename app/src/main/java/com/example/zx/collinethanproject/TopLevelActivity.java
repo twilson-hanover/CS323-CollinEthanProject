8d7a40bee2a83536b1ec65a13e63aad0e1352225
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2018-05-24 11:40:59 -0400

    Beginning to implement an ActionBar

--- app/src/main/java/com/example/zx/collinethanproject/TopLevelActivity.java
+++ app/src/main/java/com/example/zx/collinethanproject/TopLevelActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toolbar;
 
 public class TopLevelActivity extends Activity {
 
@@ -11,6 +12,7 @@ public class TopLevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
     }
 
     public void startGame(View v){
